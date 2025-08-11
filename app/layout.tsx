9561a5e39f2351b8ce83211b5a09d175c28b682d
alldy28 <aldimaulana19@gmail.com>
2025-08-11 16:20:17 +0700

    kembali ke awal

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,21 +15,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "app.silverium.id",
   description: "Silverium Indonesia",
-  // Mengatur ikon untuk favicon browser
-  icons: {
-    icon: "/logo-Silverium.png", // Pastikan file ini ada di folder /public
-  },
-  // PENAMBAHAN: Mengatur gambar untuk pratinjau link (Open Graph)
-  openGraph: {
-    images: [
-      {
-        url: "/logo-Silverium.png", // Gunakan path absolut dari folder /public
-        width: 800, // Opsional, tapi disarankan
-        height: 600, // Opsional, tapi disarankan
-        alt: "Logo Silverium",
-      },
-    ],
-  },
 };
 
 export default function RootLayout({
