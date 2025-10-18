38f2b170bde78e0c4440b52a59a0b94445d04c43
alldy28 <aldimaulana19@gmail.com>
2025-10-18 17:35:24 +0700

    Fix: Tambah file deklarasi TypeScript untuk A-Frame

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,18 +1,12 @@
-// Ini "mengajari" TypeScript bahwa tag A-Frame ada
-// dan bisa menerima properti apa saja (any).
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'a-scene': any;
-      'a-assets': any;
-      'a-camera': any;
-      'a-entity': any;
-      'a-plane': any;
-    }
+// Memberi tahu TypeScript agar mengenali tag A-Frame
+declare namespace JSX {
+  interface IntrinsicElements {
+    'a-scene': any
+    'a-assets': any
+    'a-camera': any
+    'a-entity': any
+    'a-plane': any
   }
 }
-
-// Baris ini diperlukan agar file ini dianggap sebagai "module"
-export {};
