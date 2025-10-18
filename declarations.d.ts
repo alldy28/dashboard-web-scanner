113c4c10d5b9ae9eedca8edbf8e11368670a1407
alldy28 <aldimaulana19@gmail.com>
2025-10-18 18:03:02 +0700

    perubahan dashbaord

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,10 +3,10 @@
 // Memberi tahu TypeScript agar mengenali tag A-Frame
 declare namespace JSX {
   interface IntrinsicElements {
-    'a-scene': any
-    'a-assets': any
-    'a-camera': any
-    'a-entity': any
-    'a-plane': any
+    'a-scene': any;
+    'a-assets': any;
+    'a-camera': any;
+    'a-entity': any;
+    'a-plane': any;
   }
 }
